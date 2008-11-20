11e1cafd3b47b6a96eefa55584a03e395bdde713
Mike West <mike@mikewest.org>
2008-11-20 17:41:56 +0100

    Starting on a factory method.  Need to do more research, as this doesn't make sense in Python.  :)

--- parser.py
+++ parser.py
@@ -6,6 +6,8 @@ class UndefinedFunction( Exception ):
 class MissingOperator( Exception ):
     pass
 
+
+
 class BaseSymbol( object ):
     def __init__( self, symbol_id, left_binding_power = 0):
         self.id     = self.value = symbol_id
@@ -16,13 +18,28 @@ class BaseSymbol( object ):
         
     def led( self, left ):
         raise MissingOperator
-        
 
-class SymbolTable( object ):
+class SymbolFactory( object ):
     def __init__( self ):
         self.__table = {}
+        
+    def get( self, symbol_id, left_binding_power = 0 ):
+        s = None
+        if ( self.__table.has_key( symbol_id ) ):
+            s = self.__table[ symbol_id ]
+            s.lbp = max( left_binding_power, s.lbp )
+        else:
+            class s( BaseSymbol ):
+                pass
+            s.__name__  = "symbol-" + symbol_id
+            s.id        = s.value   = symbol_id
+            s.lbp       = left_binding_power
+            self.__table[ symbol_id ] = s
+        
+        return s
 
-    
+
+
 
 class Parser( object ):
     def __init__( self, string_to_parse ):
