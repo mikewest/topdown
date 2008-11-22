d85cb0e7d7b48e407e2e392330761b194ab6b6c5
Mike West <mike@mikewest.org>
2008-11-22 16:09:49 +0100

    You knew it was coming.  I'm still stumped on factory pattern in pythonm so I'm starting on a CSS tokenizer for the JS tokenizer to play with.

---
+++ tests/TestCSSTokenizer.py
@@ -0,0 +1,31 @@
+import sys
+sys.path.append('..')
+
+from tokenizer import Token, TokenList
+from csstokenizer import CSSTokenizer
+from tokenexceptions import *
+import unittest
+
+if __name__ == "__main__":
+    class KnownValues( unittest.TestCase ):
+        IdentifierKnownValues = [
+                                    (
+                                        '#id', Token( 'ID', 'id' )
+                                    ),
+                                    (
+                                        '.class', Token( 'CLASS', 'class' )
+                                    ),
+                                    (
+                                        '#id .class', ( Token( 'ID', 'id' ), Token( 'CLASS', 'class' ) )
+                                    ),
+                                ]
+        def assertEqualTokens( self, a, b ):
+            return self.assertEqual( TokenList( a ), b )
+            
+        def testCSSTokenizerKnownIdentifierValues( self ):
+            """CSSTokenizer.tokenize should give known result with known input"""
+            for string, tokens in self.IdentifierKnownValues:
+                result = CSSTokenizer( string ).tokenize()
+                self.assertEqualTokens( tokens, result )
+                
+    unittest.main()
